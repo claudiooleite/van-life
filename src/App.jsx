0e3f654b243bd188256df88552d63de283227859
Claudio Leite <claudio.leite@hotmail.com>
2024-10-28 10:54:00 +0100

    routes and server added, styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Vans from './pages/Vans'
 import './App.css'
+import { makeServer } from './server'
+
+// Initialize the server
+makeServer()
+
 
 function App() {
-
-
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to='/'  >#VANLIFE</Link>
+        <nav>
+          <Link to='/about'> About</Link>
+          <Link to='/vans'> Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+
+
+
   )
 }
 
